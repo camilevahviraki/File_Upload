d640cde30dc86b7af819412398dd52247433bc79
camilevahviraki <camilevahviraki@gmail.com>
2022-11-10 21:05:43 +0200

    Sart-App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
+/* eslint-disable no-fallthrough */
+import {useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [gallery, setGallery] = useState({});
+  const [fyleType, setFileType] = useState('image');
+  const [multiple, setMultiple] = useState(false);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // ///////////////////////////////////////// Upload the files
+    // axios.post('URL', {
+    //   files: e.target.gallery.files,
+    // }, 
+    // {
+    //   headers: {
+    //     'Content-Type': 'multipart/form-data',
+    //   },
+    // } ).then(response => console.log(response));
+    // ////////////////////////////////////////// Upload the files
+  }
+
+  const handleSelect = (e) => {
+     const choosen = e.target.value;
+
+     if(choosen === 'Image'){
+      setFileType('image');
+      setMultiple(false);
+     }else if(choosen === 'Gallery'){
+      setFileType('image');
+      setMultiple(true);
+     }else if(choosen === 'Video'){
+      setFileType('video');
+      setMultiple(false);
+     }
+  }
+
+  const handlePreview = (e) => {
+     setGallery(e.target.files);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className='Images'>
+          {
+          Object.keys(gallery).map((keyName, i) => (
+            <img src={URL.createObjectURL(gallery[keyName])} alt='' />
+          ))
+          }
+       </div>
+        <form onSubmit={handleSubmit} className="form">
+           <select onChange={handleSelect}>
+            <option value='Image'>Image</option>
+            <option value='Gallery'>Gallery</option>
+            <option value='Video'>Video</option>
+           </select>
+           {
+            multiple ? (
+              <input 
+              type='file' 
+              id="gallery"
+              name="gallery"
+              accept={`${fyleType}/*`}
+              onChange={handlePreview}
+              multiple
+              />
+            ):(
+              <input 
+              type='file' 
+              id="gallery"
+              name="gallery"
+              accept={`${fyleType}/*`}
+              onChange={handlePreview}
+              />
+            )
+           }
+           <input type='submit' value='Upload'/>
+        </form>
     </div>
   );
 }
